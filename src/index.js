16f30ecf3f6299428ae5017d707da48c18ee4da5
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2022-10-07 15:18:54 -0300

    Implementação do arquivo de rotas básico

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import SignIn from './pages/SignIn';
+import MainRoutes from './routes';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SignIn />
+    <BrowserRouter>
+      <MainRoutes />
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+
+function MainRoutes() {
+    return (
+        <Routes>
+            <Route path='/' element={<SignIn />} />
+        </Routes>
+    );
+}
+
+export default MainRoutes;
