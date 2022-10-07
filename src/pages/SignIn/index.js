68217f18cc4c395bfaadfb84fd78f08f15f90ce4
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2022-10-07 17:00:09 -0300

    Implementação da página de Login - estilização

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,41 @@
 import './styles.css';
+import Logo from '../../assets/logo.svg';
 
 function SignIn() {
+  
   return (
-    <div>
-      <h1>SignIn</h1>
+    <div className='container-sign-in'>
+      <img src={Logo} alt="Logo" className='logo' />
+
+      <div className='content-sign-in'>
+        <div className='left'>
+          <h1>Controle suas <span>finanças</span>,
+            sem planilha chata.
+          </h1>
+          <h3>Organizar as suas finanças nunca foi tão fácil, com o DINDIN,
+            você tem tudo num único lugar e em um clique de distância.
+          </h3>
+          <button className='btn-purple btn-big'>
+            Cadastre-se
+          </button>
+        </div>
+        <div className='right'>
+          <form>
+            <h2>Login</h2>
+            <div className='container-inputs'>
+              <label htmlFor='email'>E-mail</label>
+              <input type="text" name="email" />
+            </div>
+            <div className='container-inputs'>
+              <label htmlFor='password'>Password</label>
+              <input type="password" name="password" />
+            </div>
+            <button className='btn-purple btn-big'>
+              Entrar
+              </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
