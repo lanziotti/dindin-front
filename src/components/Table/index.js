3829109b0769a015b8da1dfd1ae61ecdd2027401
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2022-10-10 18:30:05 -0300

    Criação do componente Confirm e suas estilizações e lógicas

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,20 +1,27 @@
+import { useState } from 'react';
+import ArrowDown from '../../assets/arrow-down.svg';
+import ArrowUp from '../../assets/arrow-up.svg';
 import DeleteIcon from '../../assets/delete-icon.svg';
 import EditIcon from '../../assets/edit-icon.svg';
-import ArrowUp from '../../assets/arrow-up.svg';
-import ArrowDown from '../../assets/arrow-down.svg';
+import Confirm from '../Confirm';
 import './styles.css';
-import { useState } from 'react';
 
 function Table() {
     const [asc, setAsc] = useState(true);
+    const [openConfirm, setOpenConfirm] = useState(false);
+
+    function handleDeleteItem() {
+        console.log('delete');
+        setOpenConfirm(false);
+    }
 
     return (
         <div className='container-table'>
             <div className='table-head'>
                 <div
-                 className='table-column-small content-date'
-                 onClick={() => setAsc(!asc)}
-                 >
+                    className='table-column-small content-date'
+                    onClick={() => setAsc(!asc)}
+                >
                     <strong>Data</strong>
                     <img src={asc ? ArrowUp : ArrowDown} alt="order" />
                 </div>
@@ -32,10 +39,19 @@ function Table() {
                     <span className='table-column-big'>Venda de um produto</span>
                     <span className='table-column-small'>Vendas</span>
                     <strong className='table-column-small'>R$ 100,00</strong>
-                    <div className='table-column-small'>
+                    <div className='table-column-small action-buttons'>
                         <img src={EditIcon} alt="edit" />
-                        <img src={DeleteIcon} alt="delete" />
+                        <img
+                            src={DeleteIcon}
+                            alt="delete"
+                            onClick={() => setOpenConfirm(true)}
+                        />
                     </div>
+                    <Confirm
+                        open={openConfirm}
+                        handleClose={() => setOpenConfirm(false)}
+                        handleConfirm={handleDeleteItem}
+                    />
                 </div>
             </div>
         </div>
