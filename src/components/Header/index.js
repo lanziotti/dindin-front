93dbcc88d58337465d9c852c459865ed673f1f39
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2022-10-12 16:13:48 -0300

    Implementação das funcionalidades de editar e atualizar o perfil do usuário

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,10 +3,11 @@ import Logo from '../../assets/logo.svg';
 import Profile from '../../assets/profile.svg';
 import Logout from '../../assets/logout.svg';
 import { useNavigate } from 'react-router-dom';
-import { clear } from '../../utils/storage';
+import { clear, getItem } from '../../utils/storage';
 
 function Header({ handleEditProfile }) {
     const navigate = useNavigate();
+    const userName = getItem('userName');
 
     function handleLogout() {
         clear();
@@ -23,7 +24,7 @@ function Header({ handleEditProfile }) {
                         className='profile-area'
                     >
                         <img src={Profile} alt="Profile" />
-                        <strong>Rodrigo</strong>
+                        <strong>{userName}</strong>
                         <img
                             src={Logout}
                             alt="Logout"
--- src/components/ProfileModal/index.js
+++ src/components/ProfileModal/index.js
@@ -1,7 +1,92 @@
 import CloseIcon from '../../assets/close-icon.svg';
 import './styles.css';
+import api from '../../services/api';
+import { useEffect, useState } from 'react';
+import { getItem, setItem } from '../../utils/storage';
+
+const defaultForm = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
 
 function ProfileModal({ open, handleClose }) {
+    const token = getItem('token');
+
+    const [form, setForm] = useState({ ...defaultForm });
+
+    function handleChangeForm({ target }) {
+        setForm({ ...form, [target.name]: target.value });
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+            if (!form.name || !form.email || !form.password || !form.confirmPassword) {
+                return;
+            }
+
+            if (form.password !== form.confirmPassword) {
+                return;
+            }
+
+            await api.put('/usuario',
+                {
+                    nome: form.name,
+                    email: form.email,
+                    senha: form.password
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
+
+            setItem('userName', form.name);
+
+            handleClose();
+            handleClearForm();
+
+        } catch (error) {
+            console.log(error.response);
+        }
+    }
+
+    function handleClearForm() {
+        setForm({ ...defaultForm });
+    }
+
+    useEffect(() => {
+
+        async function loadUserProfile() {
+            try {
+                const response = await api.get('/usuario', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+
+                const { nome, email } = response.data;
+
+                setForm({
+                    ...form,
+                    name: nome,
+                    email: email,
+                });
+
+
+            } catch (error) {
+                console.log(error.response);
+            }
+        }
+        if (open) {
+            loadUserProfile();
+        }
+    }, [open]);
+
     return (
         <>
             {open &&
@@ -14,22 +99,46 @@ function ProfileModal({ open, handleClose }) {
                             onClick={handleClose}
                         />
                         <h2>Editar Perfil</h2>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <div className='container-inputs'>
                                 <label>Nome</label>
-                                <input type="text" />
+                                <input
+                                    type="text"
+                                    name='name'
+                                    value={form.name}
+                                    onChange={handleChangeForm}
+                                    required
+                                />
                             </div>
                             <div className='container-inputs'>
                                 <label>E-mail</label>
-                                <input type="text" />
+                                <input
+                                    type="text"
+                                    name='email'
+                                    value={form.email}
+                                    onChange={handleChangeForm}
+                                    required
+                                />
                             </div>
                             <div className='container-inputs'>
                                 <label>Senha</label>
-                                <input type="password" />
+                                <input
+                                    type="password"
+                                    name='password'
+                                    value={form.password}
+                                    onChange={handleChangeForm}
+                                    required
+                                />
                             </div>
                             <div className='container-inputs'>
                                 <label>Confirmação de senha</label>
-                                <input type="password" />
+                                <input
+                                    type="password"
+                                    name='confirmPassword'
+                                    value={form.confirmPassword}
+                                    onChange={handleChangeForm}
+                                    required
+                                />
                             </div>
                             <button className='btn-purple btn-small'>
                                 Confirmar
